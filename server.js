be8990f8233b78560a88140734ed504166c49c19
Vanquoc0201 <vovanquoc0201@gmail.com>
2025-04-22 22:29:12 +0700

    Complete Setup Source

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import logger from "./src/common/winston/init.winston";
+import logApi from "./src/common/morgan/init.morgan";
+import cors from "cors";
+import { handleError } from "./src/common/helpers/error.helper";
+import rootRouter from "./src/routers/root.router";
+const app = express();
+app.use(express.json());
+app.use(logApi());
+app.use(handleError);
+app.use(
+    cors({
+      origin: ["http://localhost:3000", "https://google.com"],
+    })
+);
+app.use(rootRouter)
+app.listen(3069, () => {
+    logger.info(`Server online at http://localhost:3069`, { tag: "SERVER" });
+});
